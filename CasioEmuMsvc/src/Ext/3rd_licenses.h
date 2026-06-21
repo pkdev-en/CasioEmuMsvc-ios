c0a1dab6bd66c828f1ab781f90fac0b8a9337c03
axd <pkdevvn@outlook.com>
2026-06-21 21:33:13 +0700

    Update 3rd_licenses.h

--- CasioEmuMsvc/src/Ext/3rd_licenses.h
+++ CasioEmuMsvc/src/Ext/3rd_licenses.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 constexpr auto licenses_str = R"aaaaaaaa(
 
 CasioEmuMsvc(or nx-U8 Emulator):
@@ -724,6 +724,22 @@ freely, subject to the following restrictions:
 2. Altered source versions must be plainly marked as such, and must not be
    misrepresented as being the original software.
 3. This notice may not be removed or altered from any source distribution.
+
+Roboto Font (https://github.com/googlefonts/roboto-2):
+
+Copyright 2011 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 
 
