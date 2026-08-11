03a691794797df761ab7c3a01db03e67cb1f73ed
axd <pkdevvn@outlook.com>
2026-08-11 08:24:27 +0700

    Update Emulator.hpp

--- CasioEmuMsvc/src/Emulator.hpp
+++ CasioEmuMsvc/src/Emulator.hpp
@@ -1,1 +1,174 @@
+#pragma once
+#include "Config.hpp"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <atomic>
+#include <cstdint>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
 
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+#include "ModelInfo.h"
+#include "ModelResourceStore.h"
+#include "QrCode.h"
+
+namespace casioemu {
+	class Chipset;
+	class CPU;
+	class MMU;
+
+	/**
+	 * A mutex that ensures that a thread cannot get the mutex right after it's released if there are another waiting thread.
+	 */
+	class FairRecursiveMutex {
+		std::mutex m;
+		std::thread::id holding;
+		int recursive_count;
+		std::queue<std::condition_variable> waiting;
+
+	public:
+		FairRecursiveMutex();
+		~FairRecursiveMutex();
+		void lock();
+		void unlock();
+	};
+
+	class Emulator {
+	public:
+		SDL_Renderer* renderer = nullptr;
+		SDL_Surface* interface_surface = nullptr;
+		SDL_Texture* interface_texture = nullptr;
+		SDL_Texture* scaled_interface_texture = nullptr;
+		int scaled_interface_texture_w = 0;
+		int scaled_interface_texture_h = 0;
+		bool interface_is_svg = false;
+		std::string interface_svg_data;
+		unsigned int cycles_per_second;
+		unsigned int timer_interval;
+		bool running, Paused;
+		bool headless = false;
+		std::atomic<bool> m_step_requested;
+		unsigned int last_frame_tick_count;
+		std::string model_path;
+		std::shared_ptr<ModelResourceStore> model_resources;
+		bool pause_on_mem_error;
+
+#ifndef CASIOEMU_CORE_WEB
+		std::atomic<bool> screenshot_requested{};
+		std::atomic<bool> mirroring_requested{};
+		std::atomic<bool> recording_requested{};
+		std::atomic<bool> recording_stop_requested{};
+		std::atomic<bool> recording_active{};
+		std::atomic<unsigned int> recording_frame_count{};
+		std::atomic<int> capture_scale{3};
+		std::atomic<uint32_t> capture_background_rgb{0xd6e3d6};
+#endif
+
+		// ── iOS / Android: các field cho CalculatorWindow và screenshot UI ────
+		// tx: texture mà emulator render calculator lên (thay vì thẳng ra SDL window).
+		// Chỉ được set khi build cho iOS/Android. Trên các platform khác luôn là nullptr.
+		SDL_Texture* tx = nullptr;
+
+		// screenshot_full_ui / screenshot_taken: dùng bởi Ui_ios.cpp
+		std::atomic<bool> screenshot_full_ui{true};
+		std::atomic<bool> screenshot_taken{false};
+
+		// recording_full_ui: dùng bởi Ui_ios.cpp
+		std::atomic<bool> recording_full_ui{true};
+
+		// calculator_as_tab: khi true, CalculatorWindow render calc trong ImGui
+		// (chỉ có ý nghĩa trên iOS/Android, desktop luôn false)
+		std::atomic<bool> calculator_as_tab{false};
+		// ─────────────────────────────────────────────────────────────────────
+
+		std::thread* tick_thread;
+
+		SpriteInfo interface_background;
+		SDL_Rect emu_rect{};
+
+		/**
+		 * A bunch of internally used methods for encapsulation purposes.
+		 */
+		void LoadModelDefition();
+		void TimerCallback();
+		void SetupLuaAPI();
+		void SetupInternals();
+		void RunStartupScript();
+
+	public:
+		ModelInfo ModelDefinition{};
+		SDL_Window* window = nullptr;
+		Emulator(std::map<std::string, std::string>& argv_map, bool Paused = false,
+			std::shared_ptr<ModelResourceStore> resources = {});
+		Emulator(ModelInfo def, bool paused = false, bool headless = true, std::string modelPath = "");
+		~Emulator();
+
+		FairRecursiveMutex access_mx;
+		HardwareId hardware_id;
+		std::map<std::string, std::string>& argv_map;
+
+		// private:
+	public:
+		/**
+		 * The cycle manager structure. This structure is reset every time the
+		 * emulator starts emulating CPU cycles and in every timer callback
+		 * it's queried for the number of cycles that need to be emulated in the
+		 * callback. This ensures that only as many cycles are emulated in a period
+		 * of time as many would be in real life.
+		 *
+		 * Note that it's assumed that the GetDelta function is called once every
+		 * timer_interval milliseconds. It's up to the timer to make sure that
+		 * there's no drift.
+		 */
+		struct Cycles {
+			void Setup(Uint64 cycles_per_second, unsigned int timer_interval);
+			void Reset();
+			Uint64 GetDelta();
+			Uint64 ticks_now, cycles_emulated, cycles_per_second;
+			unsigned int timer_interval;
+		} cycles;
+		/**
+		 * A reference to the emulator chipset. This object holds all CPU, MMU, memory and
+		 * peripheral state. The emulator interfaces with the chipset by issuing interrupts
+		 * and rendering the screen buffer. It may also read internal state for testing purposes.
+		 */
+		Chipset& chipset;
+
+		float BatteryVoltage, SolarPanelVoltage;
+		QrCodeCapture qr_code;
+
+		bool Running();
+		void HandleMemoryError();
+		void Shutdown();
+		void Tick();
+		/**
+		 * Called when SDL_WINDOWEVENT_EXPOSED event is received. Does not re-frame.
+		 */
+		void Repaint();
+		void Frame();
+		void WindowResize(int width, int height);
+		void ExecuteCommand(std::string command);
+		unsigned int GetCyclesPerSecond();
+		void SetClockSpeed(float speed);
+		bool GetPaused();
+		void SetPaused(bool paused);
+		void RequestStep();
+		void UIEvent(SDL_Event event);
+		SDL_Renderer* GetRenderer();
+		SDL_Texture* GetInterfaceTexture();
+		std::string GetModelFilePath(std::string relative_path) const;
+		bool HasModelResource(const std::string& name) const;
+		std::vector<std::uint8_t> ReadModelResource(const std::string& name) const;
+		void WriteModelSessionResource(const std::string& name, const std::vector<std::uint8_t>& data);
+		bool IsMemoryModel() const { return static_cast<bool>(model_resources); }
+
+		friend class CPU;
+		friend class MMU;
+	};
+} // namespace casioemu
